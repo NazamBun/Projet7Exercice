a8fc0e5a18e77cb8963dc84bdae59c0e65a3838c
Bun Nazam <bun.nazam@gmail.com>
2024-10-31 16:13:01 +0100

    add initial dataset in AppDatabase onCreate callback

--- app/src/main/java/com/openclassrooms/arista/data/AppDatabase.kt
+++ app/src/main/java/com/openclassrooms/arista/data/AppDatabase.kt
@@ -4,33 +4,73 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.openclassrooms.arista.data.dao.ExerciseDtoDao
 import com.openclassrooms.arista.data.dao.SleepDtoDao
 import com.openclassrooms.arista.data.dao.UserDtoDao
 import com.openclassrooms.arista.data.entity.ExerciseDto
 import com.openclassrooms.arista.data.entity.SleepDto
 import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
-@Database(entities = [UserDto::class, ExerciseDto::class, SleepDto::class], version = 1, exportSchema = false)
-        abstract class AppDataBase : RoomDatabase() {
-            abstract fun userDtoDao(): UserDtoDao
-            abstract fun exerciseDtoDao(): ExerciseDtoDao
-            abstract fun sleepDtoDao(): SleepDtoDao
+@Database(
+    entities = [UserDto::class, ExerciseDto::class, SleepDto::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDataBase : RoomDatabase() {
+    abstract fun userDtoDao(): UserDtoDao
+    abstract fun exerciseDtoDao(): ExerciseDtoDao
+    abstract fun sleepDtoDao(): SleepDtoDao
 
-            companion object {
-                @Volatile
-                private var INSTANCE: AppDataBase? = null
-
-                fun getDatabase(context: Context): AppDataBase {
-                    return INSTANCE ?: synchronized(this) {
-                        val instance = Room.databaseBuilder(
-                            context.applicationContext,
-                            AppDataBase::class.java,
-                            "app_database"
-                        ).build()
-                        INSTANCE = instance
-                        instance
-                    }
+    private class AppDataBaseCallback(
+        private val scope: CoroutineScope
+    ) : Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.sleepDtoDao(), database.userDtoDao())
                 }
             }
         }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+
+        fun getDatabase(context: Context): AppDataBase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDataBase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+        suspend fun populateDatabase(sleepDao: SleepDtoDao, userDtoDao: UserDtoDao) {
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime = LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant()
+                        .toEpochMilli(), duration = 480, quality = 4
+                )
+            )
+
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime =
+                    LocalDateTime.now().minusDays(2).atZone(ZoneOffset.UTC).toInstant()
+                        .toEpochMilli(), duration = 450, quality = 3
+
+                )
+            )
+        }
+    }
+}
