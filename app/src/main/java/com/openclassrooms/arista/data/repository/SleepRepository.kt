65c6e2dc55469431eb1a257da66dc9b14c673d8e
Bun Nazam <bun.nazam@gmail.com>
2024-11-01 16:12:55 +0100

    feat: connect Sleep repository with DAO

--- app/src/main/java/com/openclassrooms/arista/data/repository/SleepRepository.kt
+++ app/src/main/java/com/openclassrooms/arista/data/repository/SleepRepository.kt
@@ -1,10 +1,28 @@
 package com.openclassrooms.arista.data.repository
 
 import com.openclassrooms.arista.data.FakeApiService
+import com.openclassrooms.arista.data.dao.SleepDtoDao
 import com.openclassrooms.arista.domain.model.Sleep
+import kotlinx.coroutines.flow.first
 
-class SleepRepository(private val apiService: FakeApiService = FakeApiService()) {
+class SleepRepository(private val sleepDao: SleepDtoDao) {
 
     // Get all sleep records
-    val allSleeps: List<Sleep> get() = apiService.getAllSleeps()
+    suspend fun getAllSleep(): List<Sleep> {
+        return sleepDao.getAllSleeps()
+            .first()
+            .map { Sleep.fromDto(it) }
+    }
+
+    suspend fun addSleep(sleep: Sleep) {
+        sleepDao.insertSleep(sleep.toDto())
+    }
+
+    suspend fun deleteSleep(sleep: Sleep) {
+        sleep.id?.let {
+            sleepDao.deleteSleepById(
+                id = sleep.id
+            )
+        }
+    }
 }
