99c5a7a6da9c5121eee756c16f620fcaf5ffaa9e
Bun Nazam <bun.nazam@gmail.com>
2024-11-01 15:59:11 +0100

    feat: add DTO conversion methods for Sleep

--- app/src/main/java/com/openclassrooms/arista/domain/model/Sleep.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/model/Sleep.kt
@@ -1,5 +1,34 @@
 package com.openclassrooms.arista.domain.model
 
+import com.openclassrooms.arista.data.entity.SleepDto
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 
-data class Sleep(@JvmField var startTime: LocalDateTime, var duration: Int, var quality: Int)
+data class Sleep(
+    @JvmField
+    val id: Long? = null,
+    var startTime: LocalDateTime,
+    var duration: Int,
+    var quality: Int
+){
+    companion object {
+        fun fromDto(dto: SleepDto): Sleep {
+            return Sleep(
+                id = dto.id,
+                startTime = LocalDateTime.ofEpochSecond(dto.startTime / 1000, 0, ZoneOffset.UTC),
+                duration = dto.duration,
+                quality = dto.quality
+            )
+        }
+    }
+
+    fun toDto(): SleepDto {
+        return SleepDto(
+            id = this.id ?: 0,
+            startTime = this.startTime.toEpochSecond(ZoneOffset.UTC) * 1000,
+            duration = duration,
+            quality = quality
+        )
+    }
+}
+
