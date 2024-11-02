52f7a945f2baa96be1d9631e08cb9512fd4fc9ea
Bun Nazam <bun.nazam@gmail.com>
2024-11-02 01:54:59 +0100

    update GetAllExercisesUseCase

--- app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
@@ -5,7 +5,12 @@ import com.openclassrooms.arista.domain.model.Exercise
 import javax.inject.Inject
 
 class GetAllExercisesUseCase @Inject constructor(private val exerciseRepository: ExerciseRepository) {
-    fun execute(): List<Exercise> {
-        return exerciseRepository.allExercises
+    suspend fun execute(): List<Exercise> {
+        return try {
+            exerciseRepository.getAllExercices()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptyList() // Retourne une liste vide ou une autre action en cas d'erreur
+        }
     }
 }
