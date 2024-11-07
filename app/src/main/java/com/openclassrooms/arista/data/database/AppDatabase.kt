1d5771c24082ecd545ff57adc580b25f07f66d91
Bun Nazam <bun.nazam@gmail.com>
2024-11-07 17:32:54 +0100

    Database création

---
+++ app/src/main/java/com/openclassrooms/arista/data/database/AppDatabase.kt
@@ -0,0 +1,90 @@
+package com.openclassrooms.arista.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.openclassrooms.arista.data.dao.ExerciceDtoDao
+import com.openclassrooms.arista.data.dao.SleepDtoDao
+import com.openclassrooms.arista.data.dao.UserDtoDao
+import com.openclassrooms.arista.data.entity.ExerciceDto
+import com.openclassrooms.arista.data.entity.SleepDto
+import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+@Database(
+    entities = [UserDto::class, SleepDto::class, ExerciceDto::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDtoDao(): UserDtoDao
+    abstract fun sleepDtoDao(): SleepDtoDao
+    abstract fun exerciseDtoDao(): ExerciceDtoDao
+
+    private class AppDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.sleepDtoDao(), database.userDtoDao())
+                }
+            }
+        }
+    }
+
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                // Create database here
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+        suspend fun populateDatabase(sleepDao: SleepDtoDao, userDtoDao: UserDtoDao) {
+
+            // Insert some sleep data
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime =
+                    LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant()
+                        .toEpochMilli(), duration = 480, quality = 4
+                )
+            )
+
+            // Insert some sleep data
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime = LocalDateTime.now().minusDays(2).atZone(ZoneOffset.UTC).toInstant()
+                        .toEpochMilli(), duration = 450, quality = 3
+                )
+            )
+
+            // Insert a user
+            userDtoDao.insertUser(
+                UserDto(
+                    name = "John Doe",
+                    email = "johndoe@example.com",
+                    password = "password123"
+                )
+            )
+        }
+    }
+}
