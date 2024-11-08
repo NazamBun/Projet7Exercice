fe1d063e64862a1276cafd450bb7ef05961004fc
Bun Nazam <bun.nazam@gmail.com>
2024-11-08 02:17:32 +0100

    Add DTO conversion methods in User class

--- app/src/main/java/com/openclassrooms/arista/domain/model/User.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/model/User.kt
@@ -1,3 +1,38 @@
 package com.openclassrooms.arista.domain.model
 
-data class User(var name: String, var email: String)
+import com.openclassrooms.arista.data.entity.UserDto
+
+data class User(
+    var id: Long, //Identifiant unique de l'uutilisateur
+    var name: String,
+    var email: String,
+    var password: String
+) {
+    /**
+     * Méthode statique qui convertit un objet UserDto (provenant de la base de données)
+     * en un objet User (modèle de domaine).
+     */
+    companion object {
+        fun fromDto(dto: UserDto): User {
+            return User(
+                id = dto.id, // Récupère l'ID depuis le DTO
+                name = dto.name, // Récupère le nom depuis le DTO
+                email = dto.email, // Récupère l'email depuis le DTO
+                password = dto.password // Récupère le mot de passe depuis le DTO
+            )
+        }
+    }
+
+    /**
+     * Méthode d'instance qui convertit un objet User en un UserDto.
+     * Cela est nécessaire pour sauvegarder les données dans la base de données.
+     */
+    fun toDto(): UserDto {
+        return UserDto(
+            id = id, // Passe l'ID à l'objet UserDto
+            name = name, // Passe le nom à l'objet UserDto
+            email = email, // Passe l'email à l'objet UserDto
+            password = password // Passe le mot de passe à l'objet UserDto
+        )
+    }
+}
